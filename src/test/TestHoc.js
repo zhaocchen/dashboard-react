6e74078bd7b1dc58a1ee03b992aa2d65397ad4ba
daaasheng <zhaocchen@163.com>
2020-01-30 21:38:13 +0800

    [add] high order component

---
+++ src/test/TestHoc.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (<div>
+            card:
+            <p>
+                名字：{this.props.name}
+            </p>
+            <p>
+                年龄: {this.props.age}
+            </p>
+        </div>)
+    }
+}
+
+const box = Comp => {
+    const age = 16;
+    // const NewComp = (props) => <Comp {...props} age={age} />
+    class NewComp extends React.Component {
+        componentDidMount() {
+            console.log('高阶处理！')
+        }
+        render() {
+            return (<Comp {...this.props} age={age}></Comp>);
+        }
+    }
+    return NewComp;
+}
+
+const withLog = Comp => {
+    console.log('withLog',Comp.name)
+    return props => <Comp {...props}></Comp>
+}
+
+// const NewCard = box(Card);
+const NewCard = box(withLog(Card));
+
+export default class TestHoc extends React.Component {
+    render() {
+        return (<div>
+            <h1>高阶组件</h1>
+            <NewCard name={"Mary"}/>
+        </div>);
+    }
+}
