56fa03ffcf1b02876267fec9913d664fbe15652a
daaasheng <zhaocchen@163.com>
2020-01-30 17:38:48 +0800

    [fill] 生命周期

--- src/components/TestLife.js
+++ src/components/TestLife.js
@@ -3,6 +3,9 @@ import React from 'react';
 class LifeCycle extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            age: 16,
+        }
         // 状态+属性初始化
         console.log("1. constructor")
     }
@@ -12,6 +15,11 @@ class LifeCycle extends React.Component{
     }
 
     componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                age: 18,
+            })
+        }, 2000)
         console.log("4. did mount")
     }
 
@@ -19,7 +27,9 @@ class LifeCycle extends React.Component{
         console.log("5. will receive props")
     }
 
-    shouldComponentUpdate() {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("对比props", this.props, nextProps);
+        console.log("对比state", this.state, nextState);
         console.log("6. should update")
         return true;
     }
@@ -64,7 +74,23 @@ class TestLife extends React.Component{
         }, 5000)
     }
     render() {
+        const styles = {
+            conclusion: {
+                border: 'solid 1px red',
+            }
+        }
         return (<div class="wrap">
+            <div style={styles.conclusion}>
+            总结：
+            <p>
+                1. 生命周期： 
+                1.1 constructor -> will mount -> render -> did mount -> will unmount
+                1.2 will receive props -> should update -> will update -> render -> did update
+            </p>
+            <p>
+                2. 组件执行顺序： 子组件 -> 父组件, 由shouldComponentUpdate更新得出结论。
+            </p>
+            </div>
             {this.state.status && <LifeCycle name={this.state.name}></LifeCycle>}
         </div>)
     }
