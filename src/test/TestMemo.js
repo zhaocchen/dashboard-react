40d1cd576901b0eaeecee760b146d76af7bf6897
daaasheng <zhaocchen@163.com>
2020-01-30 19:11:54 +0800

    [fill] memo

--- src/test/TestMemo.js
+++ src/test/TestMemo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Comp (props) {
-    return (<div>memo</div>)
+    return (<div>memo
+
+        <p>memo(函数式组件) = pureComponent</p>
+        <p>base: reactV16.6+</p>
+    </div>)
 }
 
 const TestMemo = React.memo(Comp);
