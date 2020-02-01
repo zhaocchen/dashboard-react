7f804c3f100fb0ac70b745196594f374bdffe9d1
daaasheng <zhaocchen@163.com>
2020-02-01 21:20:42 +0800

    [fill] form

--- src/test/TestForm.js
+++ src/test/TestForm.js
@@ -1,44 +1,135 @@
 import React from 'react'
 
-class RadioGroup extends React.Component {
-    render() {
-        return (<div>
-            {console.log(this.props.children)}
-            {
-                React.Children.map(this.props.children, child => {
-                    console.log(this.props.name)
-                    // child.props.name = this.props.name
-                    const NewChild = React.cloneElement(child, {
-                        name: this.props.name
+
+
+// hoc: 事件处理+数据收集+校验
+
+function FormCreate(Comp) {
+    class NewComp extends React.Component {
+        constructor(props) {
+            super(props);
+            this.options = {};
+            this.state = {};
+        }
+
+        validateField = field => {
+            const { rules } = this.options[field]
+            // console.log('rules', rules)
+            const res = !rules.some(rule => {
+                // validate required
+                if (rule.required && !this.state[field]) {
+                    // fill error message
+                    this.setState({
+                        [field+'Message']: rule.message
                     })
-                    return NewChild
+                    return true
+                }
+            })
+            // clear error message
+            if (res) {
+                this.setState({
+                    [field+'Message']: ""
                 })
             }
-        </div>)
+            return res
+        }
+    
+        handleChange = e => {
+            const { name, value } = e.target
+            // console.log(name, value)
+            this.setState({
+                [name]: value
+            }, () => {
+                this.validateField(name)
+                // console.log(this.options, this.state)
+            })
+        }
+
+        getFieldDec = (field, option) => {
+            this.options[field] = option
+            return InputComp => (<div>
+                {React.cloneElement(InputComp, {
+                    name: field,
+                    value: this.state[field] || "",
+                    onChange: this.handleChange
+                })}
+                {/* error message */}
+                {this.state[field+'Message'] && (
+                    <p style={{color: 'red'}}>
+                        {this.state[field+'Message']}
+                    </p>
+                )}
+            </div>)
+        }
+
+        validateAll = cb => {
+            const resList = Object.keys(this.options).map(field => {
+                return this.validateField(field)
+            })
+            const res = resList.every(v => v === true)
+            cb(res, this.state)
+        }
+
+        render() {
+            return <Comp {...this.props}
+            getFieldDec={this.getFieldDec}
+            validateAll={this.validateAll}
+            ></Comp>
+        }
     }
+    return NewComp
 }
 
-class Radio extends React.Component {
+@FormCreate
+class Form extends React.Component {
+    onSubmit = () => {
+        // alert('提交！')
+        const { validateAll } = this.props;
+        validateAll((isValid, data) => {
+            if (isValid) {
+                alert("提交成功")
+            } else {
+                console.log("校验失败")
+            }
+        })
+    }
     render() {
-        const {children, ...rest} = this.props;
-        // console.log(this.props, children, rest)
-        return (<p>            
-            <label>
-                <input type="radio" {...rest} />
-                {children}
-            </label>
-        </p>);
+        const styles = {
+            form: {
+                border: 'solid 1px red',
+            }
+        }
+        const { getFieldDec } = this.props
+        // console.log(this.props.children)
+        return <div style={styles.form}>
+            {/* {this.props.children}             */}
+            {getFieldDec("username", {
+                rules: [{
+                    required: true,
+                    message: "请输入用户名"
+                }]
+            })(<input type="text" />)}
+            {getFieldDec("password", {
+                rules: [{
+                    required: true,
+                    message: "请输入密码"
+                }]
+            })(<input type="password" />)}
+                
+                <button onClick={this.onSubmit}>
+                    登录
+                </button>
+        </div>;
     }
 }
 
 export default class TestForm extends React.Component {
     render() {
         return (<div>
-            <RadioGroup name="favorite">
-                <Radio value="apple">apple</Radio>
-                <Radio value="banana">banana</Radio>
-                <Radio value="orange">orange</Radio>
-            </RadioGroup>
+            <h1>实现表单</h1>
+            <Form>
+                
+            </Form>
         </div>);
     }
 }
