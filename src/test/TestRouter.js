94b11aa8b29b25de9b795db91cd26e62581d1e24
daaasheng <zhaocchen@163.com>
2020-02-02 16:31:43 +0800

    [add] router

---
+++ src/test/TestRouter.js
@@ -0,0 +1,61 @@
+import  React from 'react'
+import {BrowserRouter, Link, Route, Switch, Redirect} from 'react-router-dom'
+// import {BrowserRouter, Link, Switch, Redirect} from 'react-router-dom'
+// import Route from './Route'
+
+function Home() {
+    return <div>Home</div>
+}
+
+function About() {
+    return <div>about</div>
+}
+
+function Login() {
+    return <div>login</div>
+}
+
+function Detail(props) {
+    console.log(props)
+return <div>
+    <p>
+        {/* 命令导航 */}
+        <button onClick={props.history.goBack}>后退</button>
+        {/* <button onClick={props.history.goForward}>前进</button> */}
+    </p>
+    <p>
+        {/* match信息 */}
+        当前id: {props.match.params.id}
+    </p>
+    <p>
+        当前位置：
+        {props.location.pathname}
+    </p>
+</div>
+}
+
+function NoMatch() {
+    return <div>
+        not found
+    </div>
+}
+
+export default class TestRouter extends React.Component{
+    render() {
+        return <BrowserRouter>router
+            <ul>
+                <Link to="/home">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/detail/34">detail(id)</Link>
+            </ul>
+            <Switch>
+                <Route path="/home" component={Home}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route path="/detail/:id" component={Detail}></Route>
+                {/* <Route component={NoMatch}></Route> */}
+                <Redirect to="/home"></Redirect>
+            </Switch>
+        </BrowserRouter>
+    }
+}
+
